2727044dc6051c10c4744ec656c844fb4ac9273b
Tom Osman <tomcharlesosman@gmail.com>
2026-01-28 14:49:58 +0100

    Balance spectral glow opacity between subtle and prominent

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,10 +11,10 @@ export default function Hero() {
       <div className="absolute top-1/3 left-1/2 -translate-x-1/2 w-[600px] h-[400px] bg-coral/5 rounded-full blur-3xl" />
       
       {/* Spectral glow behind logo */}
-      <div className="absolute top-[18%] left-1/2 -translate-x-1/2 w-[350px] h-[350px] bg-amber/20 rounded-full blur-[100px] pointer-events-none" />
+      <div className="absolute top-[18%] left-1/2 -translate-x-1/2 w-[320px] h-[320px] bg-amber/14 rounded-full blur-[100px] pointer-events-none" />
       
       {/* Spectral glow behind headline */}
-      <div className="absolute top-[40%] left-1/2 -translate-x-1/2 w-[600px] h-[250px] bg-amber/15 rounded-full blur-[80px] pointer-events-none" />
+      <div className="absolute top-[40%] left-1/2 -translate-x-1/2 w-[550px] h-[220px] bg-amber/11 rounded-full blur-[80px] pointer-events-none" />
       
       {/* Outer frame */}
       <div className="absolute inset-6 border border-wood/20 rounded-[24px] pointer-events-none" />
